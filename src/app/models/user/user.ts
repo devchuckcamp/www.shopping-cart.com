ff354a30ed482c6e09d49642959643f992f38397
Chuckie Granada <devchuckcamp@gmail.com>
2017-12-22 23:10:00 +0800

    Added User Model

---
+++ src/app/models/user/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    username: string;
+    name: string;
+    email: string;
+    password: any;
+    status: number;
+    role_id: number;
+    doctor: Array<any>;
+    secretary: Array<any>;
+    pharmacist: Array<any>;
+    patient: Array<any>;
+    firstName: string;
+    length:number;
+}
