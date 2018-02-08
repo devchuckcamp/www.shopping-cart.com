e1d3876bd72601a1d553bf0cdf01d1c60059ddbd
Chuckie Granada <devchuckcamp@gmail.com>
2018-02-08 13:43:10 +0800

    Indentions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,12 @@ const routes: Routes = [
     // // { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
     // { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
     // { path: '**', redirectTo: 'not-found' }
+
+
+
 ];
+
+
 
 
 
